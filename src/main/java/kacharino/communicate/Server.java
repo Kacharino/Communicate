9cfad4deeb99af5bd112557e55da1970cd54fa90
Kacha Narimanidze <k.narimanidze2000@gmail.com>
2024-11-30 09:24:32 +0100

    Initial commit

--- src/main/java/kacharino/communicate/Server.java
+++ src/main/java/kacharino/communicate/Server.java
@@ -1,4 +1,33 @@
 package kacharino.communicate;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+
+
+    @Override
+    public void run() {
+
+        try {
+            ServerSocket server = new ServerSocket(9696);
+            Socket client = server.accept();
+        } catch (IOException e) {
+
+        }
+    }
+
+    class ConnectionHandler implements Runnable{
+        private Socket client;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+        @Override
+        public void run() {
+
+        }
+    }
+
 }
